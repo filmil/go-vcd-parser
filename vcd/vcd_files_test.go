18572a797268740f05ee34d5c416052c41101e5e
Filip Filmar <filmil@gmail.com>
2024-10-24 02:03:54 -0700

    feat: make the setup work in go as well

--- vcd/vcd_files_test.go
+++ vcd/vcd_files_test.go
@@ -3,20 +3,23 @@ package vcd
 import (
 	"bufio"
 	"os"
+	"path"
+	"strings"
 	"testing"
 )
 
+// This test runs in the directory //vcd.  See BUILD.bazel file for details.
 func TestVCDFiles(t *testing.T) {
 	t.Parallel()
-	entries, err := os.ReadDir(".")
+	entries, err := os.ReadDir("samples")
 	if err != nil {
 		t.Fatalf("could not read dir: %v", err)
 	}
 	for _, entry := range entries {
 		entry := entry
 		t.Run(entry.Name(), func(t *testing.T) {
-			name := entry.Name()
-			if name == "." || name == ".." || entry.IsDir() {
+			name := path.Join("samples", entry.Name())
+			if name == "." || name == ".." || !strings.HasSuffix(name, ".vcd") || entry.IsDir() {
 				return
 			}
 			f, err := os.Open(name)
