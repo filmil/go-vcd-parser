21ff9cb55ce1f04a6cca797a83a7cb1ecd04e7e3
Filip Filmar <filmil@gmail.com>
2025-05-21 07:49:24 +0000

    feat: autogeneration completed

--- bin/sqlite2drawtiming/main.go
+++ bin/sqlite2drawtiming/main.go
@@ -12,22 +12,39 @@ import (
 )
 
 type ArrayValue struct {
-	v []string
+	v map[string]string
 }
 
 var _ flag.Value = (*ArrayValue)(nil)
 
+var separator string
+
 func (self ArrayValue) String() string {
 	ret := []string{}
-	for _, s := range self.v {
-		ret = append(ret, fmt.Sprintf("'%s'", s))
+	for k := range self.v {
+		ret = append(ret, fmt.Sprintf("'%s'", k))
 	}
 	return strings.Join(ret, ",")
 }
 
 func (self *ArrayValue) Set(v string) error {
-	self.v = append(self.v, v)
+	if self.v == nil {
+		self.v = map[string]string{}
+	}
+	s := strings.Split(v, separator)
+	if len(s) == 2 {
+		self.v[s[0]] = s[1]
+	} else {
+		self.v[v] = v
+	}
 	return nil
+}
+
+func (self *ArrayValue) Get(v string) string {
+	if m, ok := self.v[v]; ok {
+		return m
+	}
+	return v
 }
 
 func main() {
@@ -35,12 +52,15 @@ func main() {
 		inDb             string
 		signals          ArrayValue
 		minTime, maxTime int
+		ndots            int
 	)
 
 	flag.StringVar(&inDb, "in", "", "Input sqlite signals database")
+	flag.StringVar(&separator, "separator", "=>", "")
 	flag.Var(&signals, "signal", "Signals to include")
-	flag.IntVar(&minTime, "min-time", 0, "")
+	flag.IntVar(&minTime, "min-time", -1, "")
 	flag.IntVar(&maxTime, "max-time", math.MaxInt, "")
+	flag.IntVar(&ndots, "ndots", 1000, "")
 	flag.Parse()
 
 	if inDb == "" {
@@ -69,28 +89,36 @@ func main() {
             v.Value
         FROM
             Svalues v
-        LEFT JOIN
+        JOIN
             Signals s
         ON
             s.Code = v.Code
-
         WHERE
+            (v.Timestamp >= ?)
+                AND
+            (v.Timestamp <= ?)
+                AND
             s.Name IN (%v)
         ORDER BY
             v.Timestamp
         ;
     `, signals.String())
 
-	fmt.Printf("%v", q)
-	rows, err := dbx.QueryContext(ctx, q)
+	rows, err := dbx.QueryContext(ctx, q, minTime, maxTime)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "database error: %v", err)
 		os.Exit(1)
 	}
-	if rows.Next() {
+
+	curr := 0
+	var stanzas []string
+	for {
+		if !rows.Next() {
+			break
+		}
 		var (
 			signal    string
-			timestamp uint
+			timestamp int
 			value     string
 		)
 		err := rows.Scan(&signal, &timestamp, &value)
@@ -98,9 +126,20 @@ func main() {
 			fmt.Fprintf(os.Stderr, "database error: %v", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%v %v %v", signal, timestamp, value)
-	} else {
-		fmt.Printf("no rows?\n")
-	}
+		if curr == timestamp {
+			s := fmt.Sprintf("%v=%v", signals.v[signal], value)
+			stanzas = append(stanzas, s)
+		} else {
+			// Figure out how many dots to put in.
+			q := (timestamp - curr) / ndots
+			fmt.Printf("%v\n", strings.Repeat(".", q))
+			fmt.Printf("# timestamp: %v\n", curr)
+			fmt.Printf("%v.\n", strings.Join(stanzas, ";"))
 
+			s := fmt.Sprintf("%v=%v", signals.v[signal], value)
+			stanzas = []string{s}
+			curr = timestamp
+		}
+
+	}
 }
